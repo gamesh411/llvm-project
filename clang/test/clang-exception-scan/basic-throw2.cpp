9fda9849f7c6a52767a2ab5df9eed05bff9e6703
Endre Fülöp <endre.fulop@sigmatechnology.com>
2025-01-03 13:01:34 +0100

    use clang::tidy::utils::ExceptionAnalyzer

--- clang/test/clang-exception-scan/basic-throw2.cpp
+++ clang/test/clang-exception-scan/basic-throw2.cpp
@@ -2,22 +2,22 @@
 // RUN: %clang_exception_scan %t.json | FileCheck %s
 
 // CHECK: Loading compilation database {{.*}}
-void f() {
+void f2() {
   throw 42;
 }
 // CHECK: Function 'f' throws exceptions:
 // CHECK-NEXT: {{.*}}'int'
 
-void g() {
+void g2() {
   try {
-    f();
+    f2();
   } catch (int) {}
 } 
 // CHECK: Function 'g' does not throw exceptions.
 
-void unknown();
+void unknown2();
 
-void h() {
-  unknown();
+void h2() {
+  unknown2();
 }
 // CHECK: Function 'h' is unknown to throw exceptions.
--- clang/tools/clang-exception-scan/ClangExceptionScan.cpp
+++ clang/tools/clang-exception-scan/ClangExceptionScan.cpp
@@ -12,23 +12,19 @@
 //
 //===--------------------------------------------------------------------===//
 
-#include "clang/AST/ASTConsumer.h"
-#include "clang/AST/ASTContext.h"
-#include "clang/CrossTU/CrossTranslationUnit.h"
 #include "clang/Tooling/JSONCompilationDatabase.h"
 #include "clang/Tooling/Tooling.h"
 #include "llvm/Support/CommandLine.h"
 #include "llvm/Support/Signals.h"
 #include <clang/Analysis/CallGraph.h>
 #include <string>
-#include <unordered_map>
 
 #include "CollectExceptionInfo.h"
+#include "ExceptionAnalyzer.cpp"
 
 using namespace llvm;
 using namespace clang;
 using namespace clang::ast_matchers;
-using namespace clang::cross_tu;
 using namespace clang::tooling;
 using namespace clang::exception_scan;
 
@@ -68,157 +64,25 @@ int main(int argc, const char **argv) {
   }
 
   llvm::outs() << "Files to process:\n";
-
-  for (const auto &F : Files)
+  for (const auto &F : Files) {
     llvm::outs() << F << '\n';
-
+  }
   llvm::outs() << "\n";
 
   ClangTool Tool(*CompDB, CompDB->getAllFiles());
 
+  ExceptionContext EC;
+
   auto ExceptionInfoCollectorFactory =
-      std::make_unique<CollectExceptionInfoActionFactory>();
+      std::make_unique<CollectExceptionInfoActionFactory>(EC);
   int result = Tool.run(ExceptionInfoCollectorFactory.get());
+
+  serializeExceptionInfo(EC, "/Users/efulop/exception_scan_output/");
+
+  reportFirstApproximation(EC, "/Users/efulop/exception_scan_output/");
 
   if (result != 0) {
     return result;
-  }
-
-  bool ShowLocation = true;
-  auto &EC = ExceptionInfoCollectorFactory->EC;
-  llvm::outs() << '\n';
-  for (const auto &FD : EC.FunctionsVisited) {
-
-    if (!EC.IsInMainFileIndex[FD])
-      continue;
-
-    const ExceptionInfo &EI = EC.ExInfoIndex[FD];
-
-    auto UncaughtThrows = EI.Throws;
-
-    auto rec_print_ei = [&](const FunctionDecl *FD, int level = 0) {
-      llvm::outs() << "Function:\n";
-
-      // ASTDumper P(llvm::outs(), /*ShowColors=*/false);
-      // P.Visit(FD->getBody());
-
-      auto rec_print_ei_impl = [&](const FunctionDecl *FD, int level,
-                                   auto &rec_ref) -> void {
-        std::string indent(2 * level, ' ');
-
-        if (level == 0) {
-          llvm::outs() << indent << "Name:\n";
-          llvm::outs() << indent << EC.ShortNameIndex[FD] << "\n";
-        }
-
-        if (!EI.Tries.empty()) {
-          llvm::outs() << indent << "tries:\n";
-
-          for (const TryInfo &Try : EI.Tries) {
-            llvm::outs() << indent << "  - " << Try.Stmt;
-            if (ShowLocation)
-              llvm::outs() << "@" << Try.Location;
-            llvm::outs() << "\n";
-          }
-        }
-
-        if (!EI.Throws.empty()) {
-          for (const ThrowInfo &Throw : EI.Throws) {
-            // if a throw statement is inside a try statement, then
-            // lets examine all the catch statements, and if a catch statement
-            // matches the throw type, then lets remove the throw statement
-            // from the list of throws.
-            // FIXME: this is ugly
-            for (const TryInfo &Try : EI.Tries) {
-              if (isInside(Throw.Expr, Try.Stmt)) {
-                for (const CatchInfo &Catch : EI.Catches) {
-                  if (isInside(Catch.Stmt, Try.Stmt)) {
-                    if (Catch.Stmt->getCaughtType() ==
-                        Throw.Expr->getSubExpr()->getType()) {
-                      const auto &AsConst = std::as_const(UncaughtThrows);
-                      UncaughtThrows.erase(
-                          std::find_if(AsConst.begin(), AsConst.end(),
-                                       [TE = Throw.Expr](const ThrowInfo &TI) {
-                                         return TE == TI.Expr;
-                                       }));
-                    }
-                  }
-                }
-              }
-            }
-          }
-
-          llvm::outs() << indent << "uncaught throws:\n";
-          for (const ThrowInfo &Throw : UncaughtThrows) {
-            llvm::outs() << indent << "  - " << Throw.Description;
-            if (ShowLocation)
-              llvm::outs() << "@" << Throw.Location;
-            llvm::outs() << "\n";
-          }
-        }
-
-        if (!EI.Catches.empty()) {
-          llvm::outs() << indent << "catches:\n";
-          for (const CatchInfo &Catch : EI.Catches) {
-            llvm::outs() << indent << "  - " << Catch.Description;
-
-            if (ShowLocation)
-              llvm::outs() << "@" << Catch.Location;
-            llvm::outs() << "\n";
-          }
-        }
-
-        if (!EI.Calls.empty()) {
-          llvm::outs() << indent << "calls:\n";
-          for (const CallInfo &Call : EI.Calls) {
-            llvm::outs() << indent << "  - " << EC.ShortNameIndex[Call.Callee];
-            if (ShowLocation)
-              llvm::outs() << "@" << Call.Location;
-            llvm::outs() << "\n";
-
-            // rec_ref(Call.Callee, level + 1, rec_ref);
-          }
-        }
-      };
-      rec_print_ei_impl(FD, level, rec_print_ei_impl);
-    };
-
-    rec_print_ei(FD);
-
-    // exception specification if noexcept false if there are any uncaught
-    // throws, otherwise it is the and-combined noexcept specification of the
-    // called functions
-    llvm::outs() << "Exception specification:\n";
-
-    llvm::outs() << "  noexcept";
-    if (FD->getExceptionSpecType() == clang::EST_BasicNoexcept ||
-        FD->getExceptionSpecType() == clang::EST_NoexceptTrue) {
-      continue;
-    }
-
-    auto PotentiallyThrowingCalls = llvm::SmallVector<const FunctionDecl *>();
-    for (const auto &Call : EI.Calls) {
-      if (Call.Callee->getExceptionSpecType() == clang::EST_BasicNoexcept ||
-          Call.Callee->getExceptionSpecType() == clang::EST_NoexceptTrue) {
-        continue;
-      }
-      PotentiallyThrowingCalls.push_back(Call.Callee);
-    }
-    if (PotentiallyThrowingCalls.empty()) {
-      continue;
-    }
-    llvm::outs() << "(";
-    bool first = true;
-    for (const auto &Call : PotentiallyThrowingCalls) {
-      if (!first) {
-        llvm::outs() << " && ";
-      }
-      first = false;
-      llvm::outs() << "noexcept(";
-      llvm::outs() << EC.ShortNameIndex[Call] << "()";
-      llvm::outs() << ")";
-    }
-    llvm::outs() << ")";
   }
 
   return result;
